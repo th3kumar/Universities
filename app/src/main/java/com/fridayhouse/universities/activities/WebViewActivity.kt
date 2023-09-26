a3a5b56a339d664b4b91e67e1d6eb1a1f81f1219
Mantu Kumar Ojha <mk420464@gmail.com>
2023-09-26 20:35:17 +0530

    added WebViewActivity and some UI changes

---
+++ app/src/main/java/com/fridayhouse/universities/activities/WebViewActivity.kt
@@ -0,0 +1,12 @@
+package com.fridayhouse.universities.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fridayhouse.universities.R
+
+class WebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+    }
+}
